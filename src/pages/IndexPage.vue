4cd9202b309b70e534db23dc820f5cbd81ae9821
lucasnegrao <lucas.negrao@gmail.com>
2023-09-04 10:20:59 -0300

    no parallel

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -198,7 +198,7 @@ export default {
       this.upload = new tus.Upload(file, {
         endpoint: 'https://tusd.alma.antiglitch.co:443/files',
         retryDelays: [0, 1000, 3000, 5000],
-        parallelUploads: 50,
+        // parallelUploads: 50,jj
         chunkSize: 5242880010,
         metadata: {
           filename: file.name,

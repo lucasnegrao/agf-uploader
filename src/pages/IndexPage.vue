90ff5e759a2315e9947453870f92fbee8682d6ba
lucasnegrao <lucas.negrao@gmail.com>
2023-09-04 00:59:52 -0300

    increased parallel to 20

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -198,7 +198,7 @@ export default {
       this.upload = new tus.Upload(file, {
         endpoint: 'https://tusd.alma.antiglitch.co:443/files',
         retryDelays: [0, 1000, 3000, 5000],
-        parallelUploads: 10,
+        parallelUploads: 20,
         metadata: {
           filename: file.name,
           filetype: file.type,
